ae5ebdf0a1099c76e1b400ad9ba7d1f4c077ba9e
Anton Chebotov <anton.chebotov@aviasales.ru>
2018-02-13 20:23:31 +0300

    Initial map region to contain all annotations

--- Kino/MapVC/MapVC.swift
+++ Kino/MapVC/MapVC.swift
@@ -11,14 +11,34 @@ class MapVC: UIViewController, MKMapViewDelegate {
 
         title = "Map"
         mapView?.register(MKAnnotationView.self, forAnnotationViewWithReuseIdentifier: kAnnotationIdentifier)
-        mapView?.addAnnotations(createAnnotations())
+        let annotations = createAnnotations()
+        mapView?.addAnnotations(annotations)
+        setInitialRegion(annotations)
     }
 
     private func createAnnotations() -> [MKAnnotation] {
-        let result = PointsFactory.allPoints().map{ Annotation(point: $0) }
-        return result
+        return PointsFactory.allPoints().map{ Annotation(point: $0) }
     }
 
+    private func setInitialRegion(_ annotations: [MKAnnotation]) {
+        var minLat = Double.greatestFiniteMagnitude
+        var maxLat = -Double.greatestFiniteMagnitude
+        var minLon = Double.greatestFiniteMagnitude
+        var maxLon = -Double.greatestFiniteMagnitude
+
+        for ann in annotations {
+            let lat = ann.coordinate.latitude;
+            let lon = ann.coordinate.longitude;
+            minLat = min(minLat, lat);
+            maxLat = max(maxLat, lat);
+            minLon = min(minLon, lon);
+            maxLon = max(maxLon, lon);
+        }
+        let center = CLLocationCoordinate2DMake((maxLat + minLat)/2.0, (maxLon + minLon)/2.0)
+        let span = MKCoordinateSpanMake((maxLat - minLat) * 2.0, (maxLon - minLon) * 2.0)
+
+        mapView?.setRegion(MKCoordinateRegionMake(center, span), animated: false)
+    }
 
     // MARK: -  MKMapViewDelegate
 
